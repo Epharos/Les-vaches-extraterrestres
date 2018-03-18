f16d4221d5d62fb1c765c404673579153525b28e
mloubet <30loubet@gmail.com>
2018-03-18 17:52:45 +0100

    Cube dégradé vert v1

--- src/fr/loubetrey/render/RenderCube.java
+++ src/fr/loubetrey/render/RenderCube.java
@@ -12,58 +12,58 @@ public class RenderCube implements Render
 		
 		GL11.glBegin(GL11.GL_QUADS);
 		{
-			GL11.glColor3f(0, 1f; 0);
+			GL11.glColor3f(0, 1f, 0);
 			GL11.glVertex3f(1.0f, 1.0f, -1.0f);
-			GL11.glColor3f(0.3f, 1f; 0.3f);
+			GL11.glColor3f(0.3f, 1f, 0.3f);
 			GL11.glVertex3f(-1.0f, 1.0f, -1.0f);
-			GL11.glColor3f(0.05f, 0.8f; 0.1f);
+			GL11.glColor3f(0.05f, 0.8f, 0.1f);
 			GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
-			GL11.glColor3f(0, 0.8f; 0);
+			GL11.glColor3f(0, 0.8f, 0);
 			GL11.glVertex3f(1.0f, 1.0f, 1.0f);
 			
-			GL11.glColor3f(0, 1f; 0);
+			GL11.glColor3f(0, 1f, 0);
 			GL11.glVertex3f(1.0f, -1.0f, 1.0f);
-			GL11.glColor3f(0.3f, 1f; 0.3f);
+			GL11.glColor3f(0.3f, 1f, 0.3f);
 			GL11.glVertex3f(-1.0f, -1.0f, 1.0f);
-			GL11.glColor3f(0.05f, 0.8f; 0.1f);
+			GL11.glColor3f(0.05f, 0.8f, 0.1f);
 			GL11.glVertex3f(-1.0f, -1.0f, -1.0f);
-			GL11.glColor3f(0, 0.8f; 0);
+			GL11.glColor3f(0, 0.8f, 0);
 			GL11.glVertex3f(1.0f, -1.0f, -1.0f);
 			
-			GL11.glColor3f(0, 1f; 0);
+			GL11.glColor3f(0, 1f, 0);
 			GL11.glVertex3f(1.0f, 1.0f, 1.0f);
-			GL11.glColor3f(0.3f, 1f; 0.3f);
+			GL11.glColor3f(0.3f, 1f, 0.3f);
 			GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
-			GL11.glColor3f(0.05f, 0.8f; 0.1f);
+			GL11.glColor3f(0.05f, 0.8f, 0.1f);
 			GL11.glVertex3f(-1.0f, -1.0f, 1.0f);
-			GL11.glColor3f(0, 0.8f; 0);
+			GL11.glColor3f(0, 0.8f, 0);
 			GL11.glVertex3f(1.0f, -1.0f, 1.0f);
 			
-			GL11.glColor3f(0, 1f; 0);
+			GL11.glColor3f(0, 1f, 0);
 			GL11.glVertex3f(1.0f, -1.0f, -1.0f);
-			GL11.glColor3f(0.3f, 1f; 0.3f);
+			GL11.glColor3f(0.3f, 1f, 0.3f);
 			GL11.glVertex3f(-1.0f, -1.0f, -1.0f);
-			GL11.glColor3f(0.05f, 0.8f; 0.1f);
+			GL11.glColor3f(0.05f, 0.8f, 0.1f);
 			GL11.glVertex3f(-1.0f, 1.0f, -1.0f);
-			GL11.glColor3f(0, 0.8f; 0);
+			GL11.glColor3f(0, 0.8f, 0);
 			GL11.glVertex3f(1.0f, 1.0f, -1.0f);
 			
-			GL11.glColor3f(0, 1f; 0);
+			GL11.glColor3f(0, 1f, 0);
 			GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
-			GL11.glColor3f(0.3f, 1f; 0.3f);
+			GL11.glColor3f(0.3f, 1f, 0.3f);
 			GL11.glVertex3f(-1.0f, 1.0f, -1.0f);
-			GL11.glColor3f(0.05f, 0.8f; 0.1f);
+			GL11.glColor3f(0.05f, 0.8f, 0.1f);
 			GL11.glVertex3f(-1.0f, -1.0f, -1.0f);
-			GL11.glColor3f(0, 0.8f; 0);
+			GL11.glColor3f(0, 0.8f, 0);
 			GL11.glVertex3f(-1.0f, -1.0f, 1.0f);
 			
-			GL11.glColor3f(0, 1f; 0);
+			GL11.glColor3f(0, 1f, 0);
 			GL11.glVertex3f(1.0f, 1.0f, -1.0f);
-			GL11.glColor3f(0.3f, 1f; 0.3f);
+			GL11.glColor3f(0.3f, 1f, 0.3f);
 			GL11.glVertex3f(1.0f, 1.0f, 1.0f);
-			GL11.glColor3f(0.05f, 0.8f; 0.1f);
+			GL11.glColor3f(0.05f, 0.8f, 0.1f);
 			GL11.glVertex3f(1.0f, -1.0f, 1.0f);
-			GL11.glColor3f(0, 0.8f; 0);
+			GL11.glColor3f(0, 0.8f, 0);
 			GL11.glVertex3f(1.0f, -1.0f, -1.0f);
 		}
 		
